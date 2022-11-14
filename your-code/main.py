1d1035e6d6c2c645199f426a8b94d969bf795820
luceromendozab <luceromendoz@gmail.com>
2022-11-14 13:26:06 +0100

    primeros9

--- your-code/main.py
+++ your-code/main.py
@@ -1,41 +1,45 @@
 #1. Import the NUMPY package under the name np.
-
-
+import numpy as np 
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
-
+'''FIRST WAY'''
+a= np.random.rand(2,3,5)
+'''SECOND WAY'''
+a2= np.random.randint(0,100, (2,3,5))
+''' LAST WAY'''
+a3 = np.random.random_sample((2,3,5))
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
-
+b = np.ones((5,2,3))
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+if a.size == b.size:
+        print("They have the same size")
+else:
+        print("they have different sizes")
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#print(a+b)
+'''we can not add a and b , because they have different shapes a is (2,3,5) and b is (5,2,3)'''
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c=np.transpose(b,(1,2,0))
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
