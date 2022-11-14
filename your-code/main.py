f3d3b2ae4b7654ef2c57f95eac5d1afd1cdf681c
JorgeLlorente <jorgellorente1994@gmail.com>
2022-11-14 18:04:48 +0100

    Lab_Numpy

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,79 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.rand(2,3,5)
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+if np.shape(a) == np.shape(b):
+        print("a y b tienen el mismo tamaño")
+else:
+        print("a y b no tienen el mismo tamaño")
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+# No se puede porque no tienen el mismo tamaño. Las matrices solo se pueden sumar elemento a elemento.
+# Es decir el (1,1,1) con el (1,1,1). Si no coinciden, no se pueden sumar.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.transpose(b,(1,2,0))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
 
+# A cada elemento de la matriz a se le ha sumado 1.
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a*c
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
+# Si porque está haciendo elemento a elemento x*1=x.
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty((2,3,5))
+print(f)
 
 
 """
@@ -75,7 +86,20 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+                for k in range(d.shape[2]):
+                        if d[i][j][k] > d_min and d[i][j][k] < d_mean:
+                                f[i][j][k] = 25
+                        elif d[i][j][k] > d_mean and d[i][j][k] < d_max:
+                                f[i][j][k] = 75
+                        elif d[i][j][k] == d_mean:
+                                f[i][j][k] = 50
+                        elif d[i][j][k] == d_min:
+                                f[i][j][k] = 0
+                        elif d[i][j][k] == d_max:
+                                f[i][j][k] = 100
+print(f)                        
 
 
 """
@@ -112,3 +136,17 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+g = np.empty((2,3,5), dtype=str)
+for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+                for k in range(d.shape[2]):
+                        if d[i][j][k] > d_min and d[i][j][k] < d_mean:
+                                g[i][j][k] = "B"
+                        elif d[i][j][k] > d_mean and d[i][j][k] < d_max:
+                                g[i][j][k] = "D"
+                        elif d[i][j][k] == d_mean:
+                                g[i][j][k] = "C"
+                        elif d[i][j][k] == d_min:
+                                g[i][j][k] = "A"
+                        elif d[i][j][k] == d_max:
+                                g[i][j][k] = "E"
