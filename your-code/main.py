e390f8f68db56b89b953aeddd8926a996533855f
Paula Elizagarate <paula.elizagarate@gmail.com>
2022-11-15 20:25:34 +0100

    avanzaaado aunque no terminado, continuaré

--- your-code/main.py
+++ your-code/main.py
@@ -28,51 +28,73 @@ a
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3), dtype = int)
 
 
 #6. Print b.
-
+b
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+    if a.shape == b.shape:
+        print("Array 'a' y 'b' tienen la misma forma")
+    else:
+        print("Array 'a' y 'b' no tienen la misma forma")
 
 
 #8. Are you able to add a and b? Why or why not?
 
+suma = np.add(a, b)
+suma
 
+#no se puede sumar dado que no tienen la misma forma, resulta inconpatible operar entre ellas.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.transpose(b, (1,2,0))
+c
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add(a,c)
+d
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+Los valores de a se encuentran por debajo de 1 ya que la muestra de random siempre se encontrará por debajo de uno, al sumarlo al array c (que es de origen 1) quedan todos por encima del 1.
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e= a*c
+e
 
 #13. Does e equal to a? Why or why not?
 
-
+    if a.shape == e.shape:
+        print("Array 'a' y 'b' tienen la misma forma")
+    else:
+        print("Array 'a' y 'b' no tienen la misma forma")
+        
+#A nivel valores contenidos dentro de los arrays, no serían iguales pero a nivel estructura si son iguales: mismo número de matrices, mismo número de filas/columnas dentro de cada matriz.
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_min= np.min(d)
+d_min
 
+d_max= np.max(d)
+d_max
+
+d_mean= np.mean(d)
+d_mean
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f= np.empty((2,3,5), dtype= int)
+f
 
 
 """
@@ -84,7 +106,14 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
+for columnas in  range(f.shape[0]):
+    for filas in range(f.shape[1]):
+        if f[columnas][filas] > d_min and f[columnas][filas] < d_mean:
+            f[columnas][filas]= 25
+        elif f[columnas][filas] > d_mean and f[columnas][filas] < d_max:
+            f[columnas][filas]= 75
+        elif f[columnas][filas] == d_mean:
+            f[columnas][filas]= 50
 
 
 
